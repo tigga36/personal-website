bb5d9e4e48ef88fe1ac418d63487c95044ed16a3
Taiga <onotaiga36@gmail.com>
2019-10-15 14:46:02 +0900

    Change btn text

--- my-app/src/pages/NavBar.js
+++ my-app/src/pages/NavBar.js
@@ -5,7 +5,7 @@ export default class NavBar extends React.Component {
   render(){
     return (
       <div className="nav-bar">
-        <h1>WEBSITE</h1>
+        <Link to="/" className="top-logo">WEBSITE</Link>
         <div className="nav-bar-btns">
           <Link to="/AboutMe" className="nav-bar-btn">About</Link>
           <Link to="/Contact" className="nav-bar-btn">Contact</Link>
