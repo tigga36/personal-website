2e18aab93b3695aa79b5312a37ee67b751392645
Taiga <onotaiga36@gmail.com>
2019-10-15 14:58:23 +0900

    Introduce simple fotter

---
+++ my-app/src/pages/Footer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Footer extends React.Component {
+  render(){
+    return (
+      <div className="footer-bar">
+        this is a footer
+      </div>
+    );
+  }
+}
