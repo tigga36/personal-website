f6363bada69828f7873471bb088c10b37aae77b5
Taiga <onotaiga36@gmail.com>
2019-10-15 14:26:31 +0900

    Implement more routes for multi-page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,64 +3,15 @@ import { Route, Switch } from 'react-router-dom'
 import './App.css';
 import HomePage from './pages/HomePage'
 import AboutMe from './pages/AboutMe'
-// import UserPage from './pages/UserPage'
+import Contact from './pages/Contact'
 
 function App() {
-  // return (
-  //   <div>
-  //     <NavBar />
-  //     <Route exact path="/" component={HomePage} />
-  //     <AboutCard />
-  //     <ContactCard />
-  //   </div>
-  // );
   return (
     <Switch>
       <Route exact path="/" component={HomePage} />
-      <Route path="/:id" component={AboutMe} />
+      <Route path="/AboutMe" component={AboutMe} />
+      <Route path="/Contact" component={Contact} />
     </Switch>
-  );
-}
-
-class NavBar extends React.Component {
-  render(){
-    return (
-      <div className="nav-bar">
-        <h1>WEBSITE</h1>
-        <div>
-          <Btn label="About" />
-          <Btn label="Contact" />
-        </div>
-      </div>
-    );
-  }
-}
-
-class AboutCard extends React.Component{
-  render(){
-    return(
-      <div className="main-card">
-        <h2>ABOUT</h2>
-        <p>SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT </p>
-      </div>
-    );
-  }
-}
-
-class ContactCard extends React.Component{
-  render(){
-    return(
-      <div className="main-card">
-        <h2>CONTACT</h2>
-        <p>SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT </p>
-      </div>
-    );
-  }
-}
-
-function Btn(props){
-  return (
-    <button className="btn">{props.label}</button>
   );
 }
 
