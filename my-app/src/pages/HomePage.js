253304c83d62519b33ecedfa7e498f280b63a1b1
Taiga <onotaiga36@gmail.com>
2019-10-15 14:25:56 +0900

    Throw in some temporal content for home page

--- my-app/src/pages/HomePage.js
+++ my-app/src/pages/HomePage.js
@@ -3,9 +3,24 @@ import NavBar from './NavBar'
 
 export default function HomePage() {
   return (
-    <div className="container">
-      <NavBar />
-      <h1>Home Page</h1>
-    </div>
+      <div>
+        <NavBar />
+        <section className="">
+          <div className="box">
+            <div className="post">
+              <h2>hey my dudes</h2>
+              <p>I'm an aspiring youngin' looking to git good.</p>
+              <p>This website is mainly for my portfolio and practicing React, but later on I'd like to add little widgets and functions related to my personal research and so on.</p>
+            </div>
+          </div>
+        </section>
+        <section className="bg">
+          <div className="box">
+            <div className="post">
+              <h2>Skills</h2>
+            </div>
+          </div>
+        </section>
+      </div>
   )
 }
