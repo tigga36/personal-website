b91919bc76c63758a157dd3947a54d89ea3f2fb5
Taiga <onotaiga36@gmail.com>
2019-10-15 14:27:58 +0900

    Minor fix on navbar text

--- my-app/src/pages/NavBar.js
+++ my-app/src/pages/NavBar.js
@@ -7,7 +7,7 @@ export default class NavBar extends React.Component {
       <div className="nav-bar">
         <h1>WEBSITE</h1>
         <div className="nav-bar-btns">
-          <Link to="/AboutMe" className="nav-bar-btn">AboutMe</Link>
+          <Link to="/AboutMe" className="nav-bar-btn">About</Link>
           <Link to="/Contact" className="nav-bar-btn">Contact</Link>
         </div>
       </div>
